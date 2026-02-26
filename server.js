24e64da715bd6a31f55379aa7d48a42617e3af5d
ChrisJ07 <chris.r.jeziorski@gmail.com>
2026-02-26 19:15:16 +0100

    Refactor server.js for better structure and clarity

--- server.js
+++ server.js
@@ -182,8 +182,14 @@ app.get("/random-song-year", (req,res)=>{
    START
 ======================= */
 
-const PORT = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("App is running!");
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
